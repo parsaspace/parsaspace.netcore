c47c9e506ca0f903fabf81c9a8ebe25406c22f87
Mojtaba Moradi <mojtabamoradi.en@gmail.com>
2018-10-21 14:38:29 +0330

    change exception

--- parsaspace.netcore/parsaspace.netcore/Exceptions/ParsaspaceException.cs
+++ parsaspace.netcore/parsaspace.netcore/Exceptions/ParsaspaceException.cs
@@ -5,24 +5,24 @@ using System.Web;
 
 namespace parsaspace.netcore.Exceptions
 {
-    public class GhasedakApiException:Exception
+    public class ParsaspaceException : Exception
     {
         /// <summary>
         /// Create an empty GhasedakApiException
         /// </summary>
-        public GhasedakApiException() { }
+        public ParsaspaceException() { }
 
         /// <summary>
         /// Create a GhasedakApiException from an error message
         /// </summary>
         /// <param name="message">Error message</param>
-        public GhasedakApiException(string message) : base(message) { }
+        public ParsaspaceException(string message) : base(message) { }
 
         /// <summary>
         /// Create a GhasedakApiException from message and another exception
         /// </summary>
         /// <param name="message">Error message</param>
         /// <param name="exception">Original Exception</param>
-        public GhasedakApiException(string message, Exception exception) : base(message, exception) { }
+        public ParsaspaceException(string message, Exception exception) : base(message, exception) { }
     }
 }
